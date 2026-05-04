be3d2989566a190af1b49bc4f1dcea63cf1d36f5
mochusPC <camelino007@gmail.com>
2026-05-04 20:26:43 +0200

    Fix CHANNEL_ID assignment to use a hardcoded value

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
-CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
+CHANNEL_ID = 1410333688549605509
 
 intents = discord.Intents.default()
 intents.message_content = True
